92e80ddf4a0e614b31664b92f63e2dd463782501
misterbander <61056749+misterbander@users.noreply.github.com>
2021-02-18 17:02:32 +0800

    feat: Select multiple cells by shift clicking or control clicking

--- core/src/main/kotlin/misterbander/commitsudoku/scene2d/SudokuGridClickListener.kt
+++ core/src/main/kotlin/misterbander/commitsudoku/scene2d/SudokuGridClickListener.kt
@@ -2,6 +2,7 @@ package misterbander.commitsudoku.scene2d
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils
 import ktx.actors.setKeyboardFocus
 
 class SudokuGridClickListener(private val grid: SudokuGrid) : ClickListener(-1)
@@ -10,7 +11,7 @@ class SudokuGridClickListener(private val grid: SudokuGrid) : ClickListener(-1)
 	{
 		super.touchDown(event, x, y, pointer, button)
 		grid.setKeyboardFocus(true)
-		if (pointer == 0)
+		if (pointer == 0 && !UIUtils.shift() && !UIUtils.ctrl())
 			grid.unselect()
 		val modifier = grid.modifier
 		if (modifier == null)
