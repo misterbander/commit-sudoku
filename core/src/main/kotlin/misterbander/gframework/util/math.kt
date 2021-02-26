d4cd4b4215ea3a844f369b54dcd7e4d48f1c20fe
Mister_Bander <kttan1999@gmail.com>
2021-02-27 01:53:08 +0800

    feat: Add circle decorations

--- core/src/main/kotlin/misterbander/gframework/util/math.kt
+++ core/src/main/kotlin/misterbander/gframework/util/math.kt
@@ -1,5 +1,33 @@
 package misterbander.gframework.util
 
+import ktx.math.vec2
+
+
+internal val temp = vec2()
+
+/**
+ * @param x1 x-component of the first point
+ * @param y1 y-component of the first point
+ * @param x2 x-component of the second point
+ * @param y2 y-component of the second point
+ * @return The angle from the first point to the second point, in degrees, where 0 is to the right and increases
+ * anti-clockwise. Angles are between 0 and 360.
+ */
+fun angle(x1: Int, y1: Int, x2: Int, y2: Int): Float = angle(x1.toFloat(), y1.toFloat(), x2.toFloat(), y2.toFloat())
+
+/**
+ * @param x1 x-component of the first point
+ * @param y1 y-component of the first point
+ * @param x2 x-component of the second point
+ * @param y2 y-component of the second point
+ * @return The angle from the first point to the second point, in degrees, where 0 is to the right and increases
+ * anti-clockwise. Angles are between 0 and 360.
+ */
+fun angle(x1: Float, y1: Float, x2: Float, y2: Float): Float
+{
+	return temp.set(x2 - x1, y2 - y1).angle()
+}
+
 /**
  * Treats a range as a cycle and "wraps" an int within the cycle.
  */
