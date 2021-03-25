6cca786e9a513218146bfd0eb63936080fe3b9ad
Mister_Bander <kttan1999@gmail.com>
2021-03-26 02:30:16 +0800

    refactor: ArrowDecoration now inherits from LineDecoration

--- core/src/main/kotlin/misterbander/commitsudoku/decorations/LineDecoration.kt
+++ core/src/main/kotlin/misterbander/commitsudoku/decorations/LineDecoration.kt
@@ -3,41 +3,90 @@ package misterbander.commitsudoku.decorations
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Intersector
+import ktx.collections.GdxArray
+import ktx.collections.gdxArrayOf
+import ktx.collections.plusAssign
 import ktx.style.get
 import misterbander.commitsudoku.scene2d.SudokuGrid
 import misterbander.gframework.util.blend
 import misterbander.gframework.util.roundedLine
 import java.io.Serializable
+import kotlin.math.abs
+import kotlin.math.max
 
-class LineDecoration(
-	grid: SudokuGrid,
-	private val i1: Int,
-	private val j1: Int,
-	private val i2: Int,
-	private val j2: Int
-) : Decoration(grid)
+open class LineDecoration(grid: SudokuGrid, startX: Int, startY: Int) : Decoration(grid)
 {
-	override var color: Color? = null
-	private val internalColor = Color()
-	override val dataObject: HashMap<String, Serializable> = hashMapOf("i1" to i1, "j1" to j1, "i2" to i2, "j2" to j2)
+	val lineCells: GdxArray<Pair<Int, Int>> = gdxArrayOf(Pair(startX, startY))
+	protected val lineJoints: GdxArray<Pair<Int, Int>> = gdxArrayOf(Pair(startX, startY))
+	private var lastJointPos = lineCells[0]
+	private var lastJointDI = 0
+	private var lastJointDJ = 0
+	val length
+		get() = lineCells.size
+	
+	override var color: Color? = game.skin["decorationcolor1"]
+	protected val internalColor = Color()
+	var isHighlighted = false
+	
+	override val dataObject: HashMap<String, Serializable>
+		get() = hashMapOf("cells" to lineCells.toArray(Pair::class.java))
+	
+	open fun addLineCell(endI: Int, endJ: Int)
+	{
+		val lastArrowCell: Pair<Int, Int> = lineCells.peek()
+		val nextToLastArrowCell: Pair<Int, Int>? = if (lineCells.size > 1) lineCells[lineCells.size - 2] else null
+		if (max(abs(endI - lastArrowCell.first), abs(endJ - lastArrowCell.second)) != 1
+			|| nextToLastArrowCell != null && endI == nextToLastArrowCell.first && endJ == nextToLastArrowCell.second)
+			return
+		lineCells += Pair(endI, endJ)
+		
+		// Check if new cell forms a line with the last arrow joint
+		val di = endI.compareTo(lastArrowCell.first)
+		val dj = endJ.compareTo(lastArrowCell.second)
+		
+		if (di == lastJointDI && dj == lastJointDJ
+			&& (endI == lastJointPos.first || endJ == lastJointPos.second || endI - lastJointPos.first == endJ - lastJointPos.second))
+			lineJoints.pop()
+		else
+		{
+			lastJointDI = endI.compareTo(lastArrowCell.first)
+			lastJointDJ = endJ.compareTo(lastArrowCell.second)
+			lastJointPos = lastArrowCell
+		}
+		lineJoints += Pair(endI, endJ)
+	}
 	
 	fun isOver(i: Int, j: Int): Boolean
 	{
-		return Intersector.distanceSegmentPoint(i1.toFloat(), j1.toFloat(), i2.toFloat(), j2.toFloat(), i.toFloat(), j.toFloat()) < 16/grid.cellSize
+		for (k in 0..lineJoints.size - 2)
+		{
+			val i1 = lineJoints[k].first
+			val j1 = lineJoints[k].second
+			val i2 = lineJoints[k + 1].first
+			val j2 = lineJoints[k + 1].second
+			if (Intersector.distanceSegmentPoint(i1.toFloat(), j1.toFloat(), i2.toFloat(), j2.toFloat(), i.toFloat(), j.toFloat()) < 16/grid.cellSize)
+				return true
+		}
+		return false
 	}
 	
 	override fun draw(batch: Batch)
 	{
+		if (lineJoints.size < 2)
+			return
 		val shapeDrawer = game.shapeDrawer
-		val x1 = grid.iToX(i1 + 0.5F)
-		val y1 = grid.jToY(j1 + 0.5F)
-		val x2 = grid.iToX(i2 + 0.5F)
-		val y2 = grid.jToY(j2 + 0.5F)
-		
-		shapeDrawer.roundedLine(
-			x1, y1, x2, y2,
-			internalColor.blend(src = color ?: game.skin["decorationcolor1"], dest = game.skin["backgroundcolor"]),
-			16F
-		)
+		for (i in 0..lineJoints.size - 2)
+		{
+			val x1 = grid.iToX(lineJoints[i].first + 0.5F)
+			val y1 = grid.jToY(lineJoints[i].second + 0.5F)
+			val x2 = grid.iToX(lineJoints[i + 1].first + 0.5F)
+			val y2 = grid.jToY(lineJoints[i + 1].second + 0.5F)
+			
+			shapeDrawer.roundedLine(
+				x1, y1, x2, y2,
+				internalColor.blend(src = if (isHighlighted) game.skin["selectedcolor"] else color!!, dest = game.skin["backgroundcolor"]),
+				16F
+			)
+		}
 	}
 }
---
+++ core/src/main/kotlin/misterbander/commitsudoku/modifiers/AbstractLineDecorationAdder.kt
@@ -0,0 +1,129 @@
+package misterbander.commitsudoku.modifiers
+
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import ktx.collections.GdxArray
+import ktx.collections.minusAssign
+import ktx.collections.plusAssign
+import misterbander.commitsudoku.decorations.LineDecoration
+import misterbander.commitsudoku.scene2d.SudokuGrid
+import misterbander.gframework.util.PersistentStateMapper
+import java.io.Serializable
+
+
+abstract class AbstractLineDecorationAdder<T : LineDecoration>(grid: SudokuGrid) : GridModfier<T>(grid)
+{
+	private val lineDecorations: GdxArray<T> = GdxArray()
+	private var currentLine: T? = null
+	
+	override val isValidIndex
+		get() = selectI in -1..9 && selectJ in -1..9
+	
+	override fun touchDown(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int)
+	{
+		updateSelect(x, y)
+		if (!isValidIndex)
+			return
+		if (button == Input.Buttons.RIGHT)
+		{
+			tryDelete()
+			return
+		}
+		
+		currentLine = newLine(grid, selectI, selectJ)
+		currentLine!!.isHighlighted = true
+		addModification(currentLine!!)
+	}
+	
+	abstract fun newLine(grid: SudokuGrid, selectI: Int, selectJ: Int) : T
+	
+	override fun touchUp(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int)
+	{
+		if (currentLine != null)
+		{
+			if (currentLine!!.length >= 2)
+				currentLine!!.isHighlighted = false
+			else
+			{
+				removeModification(currentLine!!)
+				tryDelete()
+			}
+		}
+		currentLine = null
+	}
+	
+	override fun touchDragged(event: InputEvent, x: Float, y: Float, pointer: Int)
+	{
+		updateSelect(x, y)
+		if (!isValidIndex)
+			return
+		val cellCenterX = grid.iToX(selectI.toFloat() + 0.5F)
+		val cellCenterY = grid.jToY(selectJ.toFloat() + 0.5F)
+		if (Vector2.dst2(x, y, cellCenterX, cellCenterY) > grid.cellSize*grid.cellSize*0.16F)
+			return
+		
+		if (currentLine != null)
+			currentLine!!.addLineCell(selectI, selectJ)
+	}
+	
+	override fun tap(event: InputEvent, x: Float, y: Float, count: Int, button: Int)
+	{
+		if (count > 1)
+		{
+			updateSelect(x, y)
+			tryDelete()
+		}
+	}
+	
+	private fun tryDelete()
+	{
+		lineDecorations.forEach {
+			if (it.isOver(selectI, selectJ))
+			{
+				removeModification(it)
+				return
+			}
+		}
+	}
+	
+	override fun addModification(modification: T)
+	{
+		lineDecorations.insert(0, modification)
+		grid.decorations += modification
+	}
+	
+	override fun removeModification(modification: T)
+	{
+		lineDecorations -= modification
+		grid.decorations -= modification
+	}
+	
+	override fun clear()
+	{
+		lineDecorations.clear()
+	}
+	
+	@Suppress("UNCHECKED_CAST")
+	override fun readState(mapper: PersistentStateMapper)
+	{
+		val lineDecorationDataObjects: Array<HashMap<String, Serializable>>? = mapper[dataObjectKey()]
+		lineDecorationDataObjects?.forEach {
+			val lineCells = it["cells"] as Array<Pair<Int, Int>>
+			val lineDecoration = newLine(grid, lineCells[0].first, lineCells[0].second)
+			lineCells.forEachIndexed { index, pair ->
+				if (index == 0)
+					return@forEachIndexed
+				lineDecoration.addLineCell(pair.first, pair.second)
+			}
+			addModification(lineDecoration)
+		}
+	}
+	
+	override fun writeState(mapper: PersistentStateMapper)
+	{
+		mapper[dataObjectKey()] = lineDecorations.map { it.dataObject }.toTypedArray()
+	}
+	
+	abstract fun dataObjectKey(): String
+}
